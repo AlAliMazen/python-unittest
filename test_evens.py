de4e320a6e45f411bc15feb9370871baa7d2b124
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-10 16:45:21 +0200

    writing a basic test files and the file to be tested

---
+++ test_evens.py
@@ -0,0 +1,14 @@
+import unittest
+
+# we have to import unittest header and inherit from it as well
+# inherit from unittest.TestCase
+class TestEvens(unittest.TestCase):
+    # pass tells the interpreter to pass the obligation of having code after the colon
+    pass
+
+    # if we comment out pass python expects a code after the colon and will throw an error
+
+
+unittest.main()
+
+
