fd70a2beeda0f416e9eb79afd2c3dd9a626f03c0
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-10 18:32:35 +0200

    Refacoring the code by using list comprehesion and truth falsy statement

--- evens.py
+++ evens.py
@@ -7,19 +7,26 @@ def even_number_of_evens(numbers):
     if the number of numbers is 0, return False
     if the number of even numbers is even, return True
     """
+    # Refactoring the code 
+    # we can optimise the code again 
     if isinstance(numbers, list):
-        if numbers == []:
-            return False
-        else: 
-            evens = 0
+        # in fact, the following line is useless, because even the list is empty it will return false at the end
+#        if numbers == []:
+#           return False
+#         else: 
+        evens = sum([1 for n in numbers if n % 2==0])
+        #using list comprehension to refactor the code
         
-        for n in numbers:
-            if n % 2==0:
-                evens+=1
+
+        #for n in numbers:
+        #    if n % 2==0:
+        #        evens+=1
         
-        if evens:
-            return evens % 2 == 0
-        return False
+        #if evens:
+        #    return evens % 2 == 0
+        #return False
+
+        return True if evens and evens % 2 == 0 else False
     else:
         raise TypeError("A list was not passed into the function")
 
@@ -31,4 +38,4 @@ want python to run only the things which we test, that is why we need to tell py
 which file is our main file.
 """
 if __name__ =='__main__':
-    print(even_number_of_evens(5))
+    print(even_number_of_evens([2,1,4]))
